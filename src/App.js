d37de1c88aa3a34d19dfd040021f31c840602474
ganeshstudyinfo <ganesh@studyinfocentre.com>
2021-05-10 22:30:35 +0545

    routing and shop pages completed and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import HomePage from './pages/homepage/homepage'
+import { Switch, Route } from 'react-router-dom';
+
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
